c495c431b670c23a25ef3e655263e1d34519f13e
Dalima Sahu <dalima62657@gmail.com>
2025-01-11 14:20:25 +0530

    day2 assignment completed

--- Day2/sc.js
+++ Day2/sc.js
@@ -1,2 +1,15 @@
-document.getElementById("content").innerText =
-  document.getElementById("content").innerText + " hello";
+
+  const button = document.querySelector(".login button"); 
+  const input = document.querySelector(".login input"); 
+  const content = document.getElementById("content");
+
+  button.addEventListener("click", inputdisplay);
+  function inputdisplay(){
+    const inputValue = input.value.trim(); 
+    if (inputValue) {
+      content.textContent = `Hello, ${inputValue}!`; 
+      input.value = ""; 
+    } else {
+      content.textContent = "Please enter your name!"; 
+    }
+  }
