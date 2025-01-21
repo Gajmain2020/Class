75f0fe56bfeed8d0e3e146bd670e1a96c990a50b
Dalima Sahu <dalima62657@gmail.com>
2025-01-21 11:43:08 +0530

    First Project Completed

--- FirstProject/src/App.jsx
+++ FirstProject/src/App.jsx
@@ -1,4 +1,4 @@
-// import "./App.css";
+import "./App.css";
 // import Card from "./components/Card";
 
 import { useState } from "react";
@@ -134,58 +134,68 @@ import { useState } from "react";
 // if(userLoggedIn)return <Feed/>
 // else return <Login />
 
-export default function App() {
-  // var loggedIn = false;
+//export default function App() {
+// var loggedIn = false;
 
-  // const [loggedIn, setLoggedIn] = useState(false);
-  // const [name, setName] = useState("");
+// const [loggedIn, setLoggedIn] = useState(false);
+// const [name, setName] = useState("");
 
-  //  const [variable, variableSetterFunction] = useState(initialState)
+//  const [variable, variableSetterFunction] = useState(initialState)
 
-  // variableSetterFunction( value ) => variable = value
-  // variableSetterFunction(callback(x)) => value retured by callback will be put on the variable x will be the holded value of variable
+// variableSetterFunction( value ) => variable = value
+// variableSetterFunction(callback(x)) => value retured by callback will be put on the variable x will be the holded value of variable
 
-  // function handleChangeClick() {
-  //   // console.log("look here", loggedIn);
-  //   // if (loggedIn === false) {
-  //   //   console.log("clickedd");
-  //   //   setLoggedIn(true);
-  //   //   setName("Jone");
-  //   // } else {
-  //   //   setLoggedIn(false);
-  //   //   setName("");
-  //   // }
+// function handleChangeClick() {
+//   // console.log("look here", loggedIn);
+//   // if (loggedIn === false) {
+//   //   console.log("clickedd");
+//   //   setLoggedIn(true);
+//   //   setName("Jone");
+//   // } else {
+//   //   setLoggedIn(false);
+//   //   setName("");
+//   // }
 
-  //   setLoggedIn((val) => !val);
-  //   setName((x) => x + "1");
-  // }
+//   setLoggedIn((val) => !val);
+//   setName((x) => x + "1");
+// }
 
-  const [user, setUser] = useState(null);
+// const [user, setUser] = useState(null);
 
-  function handleFetchData() {
-    console.log("fetchData");
+// function handleFetchData() {
+//   console.log("fetchData");
 
-    fetch("https://dummyjson.com/users/1")
-      .then((res) => res.json())
-      .then((res) => setUser(res));
-  }
+//   fetch("https://dummyjson.com/users/1")
+//     .then((res) => res.json())
+//     .then((res) => setUser(res));
+// }
 
-  console.log("currently user", user);
+// console.log("currently user", user);
 
-  return (
-    <>
-      {/* <button onClick={handleChangeClick}>login</button> */}
-      {/* {loggedIn ? <div>Logged In</div> : <div>Login karr le bhai</div>} */}
-      {/* <div>Name = {name !== "" && name}</div> */}
-      <button onClick={handleFetchData}>{user ? "reset" : "fetch data"}</button>
+// return (
+//<>
+{
+  /* <button onClick={handleChangeClick}>login</button> */
+}
+{
+  /* {loggedIn ? <div>Logged In</div> : <div>Login karr le bhai</div>} */
+}
+{
+  /* <div>Name = {name !== "" && name}</div> */
+}
+{
+  /* <button onClick={handleFetchData}>{user ? "reset" : "fetch data"}</button>
 
       {!user ? (
         <div>please fetch data</div>
       ) : (
         <div>user is there and here i will show card</div>
-      )}
-    </>
+      )} */
+}
+{
+  /* </>
   );
+} */
 }
 
 // create component for card and show it only if the user is there (use above fetch call to get dummy user)
@@ -194,3 +204,118 @@ export default function App() {
 // 1. create a counter app with +3 +1 button and -1 buttons
 // 2. create card component and above task
 // 3. try creating todo
+function Card({ user }) {
+  return (
+    <div className="card">
+      <h3>User Information</h3>
+      <p>
+        <b>Name:</b>
+        {user.name}
+      </p>
+      <p>
+        <b>Email:</b>
+        {user.email}
+      </p>
+      <p>
+        <b>Age:</b>
+        {user.age}
+      </p>
+    </div>
+  );
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  return (
+    <div className="counter">
+      <h3>Counter</h3>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 3)}>+3</button>
+      <button onClick={() => setCount(count + 1)}>+1</button>
+      <button onClick={() => setCount(count - 1)}>-1</button>
+    </div>
+  );
+}
+
+function TodoApp() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  const addTodo = () => {
+    if (newTodo.trim === "") return;
+    setTodos([...todos, { id: Date.now(), text: newTodo, done: false }]);
+    setTodos("");
+  };
+
+  const toggleTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, done: !todo.done } : todo
+      )
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+  return (
+    <div className="todo">
+      <h3>Todo List</h3>
+      <input
+        type="text"
+        placeholder="Enter a task"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+      />
+      <button onClick={addTodo}>Add</button>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <span
+              style={{
+                textDecoration: todo.done ? "line-through" : "none",
+                cursor: "pointer",
+              }}
+              onClick={() => toggleTodo(todo.id)}
+            >
+              {todo.text}
+            </span>
+            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default function App() {
+  const [user, setUser] = useState(null);
+  const fetchUser = () => {
+    if (user) {
+      setUser(null);
+    } else {
+      fetch("https://dummyjson.com/users/1")
+        .then((res) => res.json())
+        .then((data) =>
+          setUser({
+            name: data.firstName + " " + data.lastName,
+            email: data.email,
+            age: data.age,
+          })
+        );
+    }
+  };
+  return (
+    <div className="app">
+      <h1>Simple React App</h1>
+      <TodoApp />
+      <Counter />
+      <div className="user-section">
+        <button onClick={fetchUser}>
+          {user ? "Reset User" : "Fetch User"}
+        </button>
+        {user && <Card user={user} />}
+      </div>
+    </div>
+  );
+}
